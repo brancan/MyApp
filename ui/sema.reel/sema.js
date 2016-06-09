58620182ce37b1a07b4243a2a5766fcc5a9b8f80
brancan <noreply>
2016-06-09 17:55:18 +0000

    Add component sema

---
+++ ui/sema.reel/sema.js
@@ -0,0 +1,16 @@
+/**
+ * @module ui/sema.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Sema
+ * @extends Component
+ */
+exports.Sema = Component.specialize(/** @lends Sema# */ {
+    constructor: {
+        value: function Sema() {
+            this.super();
+        }
+    }
+});
